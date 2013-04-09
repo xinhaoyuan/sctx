6661a47f35e73a64c9ba9f432de068fe8ef80da0
xy2189 <xinhaoyuan@gmail.com>
2013-04-09 16:42:26 -0400

    initial wifi rules

--- xy2189/sctx/src/org/sctx/SmartContext.java
+++ xy2189/sctx/src/org/sctx/SmartContext.java
@@ -1,5 +1,6 @@
 package org.sctx;
 
+import java.util.HashMap;
 import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.Intent;
@@ -15,13 +16,57 @@ public class SmartContext extends Service {
 	
 	WifiContext wifi;
 	
+	HashMap<String, Integer> contextRef;
+	
+	void getContext(String name) {
+		if (contextRef.containsKey(name)) {
+			contextRef.put(name, contextRef.get(name) + 1);
+		}
+		else {
+			contextRef.put(name, 1);
+			enterContext(name);
+		}
+	}
+	
+	void putContext(String name) {
+		if (contextRef.containsKey(name)) {
+			int value = contextRef.get(name);
+			if (value == 1)
+			{
+				contextRef.remove(name);
+				leaveContext(name);
+			} else {
+				contextRef.put(name, value - 1);
+			}
+		}
+	}
+	
+	void enterContext(String name) {
+		Util.log("Enter context " + name);
+	}
+	
+	void leaveContext(String name) {
+		Util.log("Leave context " + name);
+	}
+	
 	@Override
 	public void onCreate() {
 		handler = new Handler();
 		msger = new Messenger(handler);
 		
+		contextRef = new HashMap<String, Integer>();
+		
 		wifi = new WifiContext(this);
 		wifi.onCreate();
+		
+		WifiRule rule = new WifiRule();
+		rule.ssid = "Columbia University";
+		rule.list_type = rule.LIST_TYPE_BLACK;
+		rule.level_threshold = -100;
+		rule.result_context = "School";
+		wifi.addRule(rule);
+		
+		wifi.bind();
 		
 		Util.log("SmartContext service created");
 	}
@@ -29,6 +74,8 @@ public class SmartContext extends Service {
 	@Override
 	public void onDestroy() {
 		Util.log("Destroying the SmartContext service");
+		
+		wifi.unbind();
 		wifi.onDestroy();
 	}
 	
--- xy2189/sctx/src/org/sctx/WifiContext.java
+++ xy2189/sctx/src/org/sctx/WifiContext.java
@@ -1,5 +1,9 @@
 package org.sctx;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 import android.content.BroadcastReceiver;
@@ -21,6 +25,9 @@ class WifiContext {
 	boolean wifiEnabled;
 	List<ScanResult> wifiLastScanResult;
 	
+	HashMap<String, HashSet<WifiRule>> ssidToRules;
+	HashSet<WifiRule> rules;
+	
 	WifiContext(SmartContext ctx) {
 		this.ctx = ctx;
 	}
@@ -28,21 +35,72 @@ class WifiContext {
 	void onCreate() {
 		wifiManager = (WifiManager)ctx.getSystemService(wifi_service);
 		wifiListener = new WifiListener();
-		
+		wifiLastScanResult = null;
+		ssidToRules = new HashMap<String, HashSet<WifiRule>>();
+		rules = new HashSet<WifiRule>();
+	}
+	
+	void bind() {
 		IntentFilter intentFilter = new IntentFilter();
 		intentFilter.addAction(wifi_state_changed_action);
 		intentFilter.addAction(wifi_rssi_changed_action);
 		ctx.registerReceiver(wifiListener, intentFilter);
 	}
 	
-	void onDestroy() {
+	void addRule(WifiRule rule) {
+		if (rules.contains(rule)) return;
+		
+		rule.lastRefCount = rule.refCount = 0;
+		
+		if (!ssidToRules.containsKey(rule.ssid)) {
+			ssidToRules.put(rule.ssid, new HashSet<WifiRule>());
+		}
+		ssidToRules.get(rule.ssid).add(rule);
+		rules.add(rule);
+	}
+	
+	void removeRule(WifiRule rule) {
+		if (!rules.contains(rule)) return;
+		HashSet<WifiRule> r = ssidToRules.get(rule.ssid);
+		r.remove(rule);
+		if (r.isEmpty()) ssidToRules.remove(rule.ssid);
+		rules.remove(rule);
+	}
+	
+	void unbind() {
 		ctx.unregisterReceiver(wifiListener);
 	}
 	
+	void onDestroy() {
+	}
+	
 	void setWifiScanResult(List<ScanResult> result) {
-		for (ScanResult item : result) {
-			Util.log("Got network: " + item.SSID + " with BSSID " + item.BSSID + " and strength " + item.level);
+		Iterator<WifiRule> it = rules.iterator();
+		while (it.hasNext()) {
+			WifiRule rule = it.next();
+			rule.lastRefCount = rule.refCount;
+			rule.refCount = 0;
 		}
+		
+		for (ScanResult item : result) {
+			if (!ssidToRules.containsKey(item.SSID)) continue;
+			it = ssidToRules.get(item.SSID).iterator();
+			while (it.hasNext())
+			{
+				WifiRule rule = it.next();
+				if (rule.match(item.SSID, item.BSSID, item.level)) ++ rule.refCount;
+			}
+		}
+		
+		it = rules.iterator();
+		while (it.hasNext()) {
+			WifiRule rule = it.next();
+			if (rule.lastRefCount == 0 && rule.refCount > 0)
+				ctx.getContext(rule.result_context);
+			else if (rule.lastRefCount > 0 && rule.refCount == 0)
+				ctx.putContext(rule.result_context);
+		}
+		
 		wifiLastScanResult = result;
 	}
 	
@@ -56,9 +114,33 @@ class WifiContext {
 			String action = intent.getAction();
 			if (action == WifiContext.wifi_state_changed_action) {
 				wifiEnabled = wifiManager.getWifiState() == WifiManager.WIFI_STATE_ENABLED;
+				// Set result to empty
+				setWifiScanResult(new LinkedList<ScanResult>());
 			} else if (action == WifiContext.wifi_rssi_changed_action && wifiEnabled) {
 				setWifiScanResult(wifiManager.getScanResults());
 			}
+		}
+	}
+}
+
+class WifiRule {
+	static final int LIST_TYPE_WHITE = 0;
+	static final int LIST_TYPE_BLACK = 1;
+	
+	String ssid;
+	String result_context;
+	int level_threshold;
+	int list_type = LIST_TYPE_BLACK;
+	HashSet<String> essid_list;
+	int lastRefCount = 0;
+	int refCount = 0;
+	
+	boolean match(String ssid, String essid, int level) {
+		if (!ssid.equals(this.ssid)) return false;
+		if (list_type == LIST_TYPE_WHITE) {
+			return (essid_list != null && essid_list.contains(essid)) && level > level_threshold;
+		} else {
+			return !(essid_list != null && essid_list.contains(essid)) && level > level_threshold;
 		}
 	}
 }
